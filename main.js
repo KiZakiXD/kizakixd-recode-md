732749d4f36e7174103a687e19093501a5e74b7c
KiZakiXD <88037146+KiZakiXD@users.noreply.github.com>
2022-01-02 14:32:29 +0700

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const figlet = require("figlet");
 const fs = require("fs");
 const P = require('pino')
 const ind = require('./help/zak')
-const { color, ZakiLog } = require("./lib/color");
+const { color, ChikaLog } = require("./lib/color");
 let setting = JSON.parse(fs.readFileSync('./config.json'));
 let sesion = `./${setting.sessionName}.json`
 const { state, saveState } = useSingleFileAuthState(sesion)
@@ -19,30 +19,30 @@ const start = async () => {
 	}), 'cyan'))
 	console.log(color('[ Created KiZaKiXD ]'))
     // set level pino ke fatal kalo ga mau nampilin log eror
-    const zaki = makeWASocket({ printQRInTerminal: true, logger: P({ level: 'debug' }), auth: state }) 
-    zaki.multi = true
-    zaki.nopref = true
-    zaki.prefa = 'anjing'
+    const chika = makeWASocket({ printQRInTerminal: true, logger: P({ level: 'debug' }), auth: state }) 
+    chika.multi = true
+    chika.nopref = true
+    chika.prefa = 'anjing'
     console.log(color('Connected....'))
-    zaki.ev.on('messages.upsert', async m => {
+    chika.ev.on('messages.upsert', async m => {
     	if (!m.messages) return
         const msg = m.messages[0]
-        require('./message/zaki')(zaki, msg, m, ind, setting)
+        require('./message/zaki')(chika, msg, m, ind, setting)
     })
 
-    zaki.ev.on('connection.update', (update) => {
+    chika.ev.on('connection.update', (update) => {
         const { connection, lastDisconnect } = update
         if (connection === 'close') {
-            console.log(ZakiLog('connection closed, try to restart'))
+            console.log(ChikaLog('connection closed, try to restart'))
             lastDisconnect.error?.output?.statusCode !== DisconnectReason.loggedOut 
             ? start()
-            : console.log(ZakiLog('Wa web terlogout.'))
+            : console.log(ChikaLog('Wa web terlogout.'))
         }
     })
 
-    zaki.ev.on('creds.update', () => saveState)
+    chika.ev.on('creds.update', () => saveState)
 
-    return zaki
+    return chika
 }
 
 start()
