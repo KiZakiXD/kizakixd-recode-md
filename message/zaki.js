df138c7dd181d6dd3b75dabaea9eb1c694c3b8b6
KiZakiXD <88037146+KiZakiXD@users.noreply.github.com>
2022-01-02 14:28:11 +0700

    Update zaki.js

--- message/zaki.js
+++ message/zaki.js
@@ -248,14 +248,15 @@ zaki.relayMessage(id, buatpesan.message, { messageId: buatpesan.key.id })
                 }
             break
             case prefix+'menu': case prefix+'help':{
-if (zaki.modelmenu == "gif") {
-await sendButton5(from, menunya, fake, await zaki.createMessage(from, {video: {url: "./media/zaki.mp4", caption: menunya}, gifPlayback: true}))
-} else if (zaki.modelmenu == "image") {
-await sendButton5(from, menunya, fake, await zaki.createMessage(from, {image: {url: setting.imgPath, caption: menunya}}))
-} else if (zaki.modelmenu ==  "loc") {
-await zaki.sendMessage(from, {
-location: { degreesLatotitude:0, degreesLongitude: 0, jpegThumbnail: fs.readFileSync("./media/menu.jpg") }})
-}
+            if (zaki.modelmenu == "gif") {
+            await sendButton5(from, menunya, fake, await zaki.createMessage(from, {video: {url: "./media/zaki.mp4", caption: menunya}, gifPlayback: true}))
+            } else if (zaki.modelmenu == "image") {
+            await sendButton5(from, menunya, fake, await zaki.createMessage(from, {image: {url: setting.imgPath, caption: menunya}}))
+            } else if (zaki.modelmenu ==  "loc") {
+            await zaki.sendMessage(from, {
+            location: { degreesLatotitude:0, degreesLongitude: 0, jpegThumbnail: fs.readFileSync("./media/menu.jpg") }})
+            }
+            }
                 break
           /*  case prefix+'allmenu': {
                 try {
